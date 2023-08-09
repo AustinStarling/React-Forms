7de1dd9cf468ad617bd8d93def6f3e06a8f77410
austin <austin.starling347@gmail.com>
2023-08-08 21:46:13 -0400

    updated form jsx

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,3 +1,18 @@
+import { useState } from 'react'
+
+const [username, setUsername] = useState("")
+const [password, setPassword] = useState("")
+const [error, setError] = useState(null)
+
 export default function SignUpForm() {
-    return <h2>Sign Up!</h2>;
+    return <><h2>Sign Up!</h2>
+    <form>
+        <label>
+            Username: <input />
+        </label>
+        <label>
+            Password: <input />
+        </label>
+        <button>Submit</button>
+    </form></>;
 }
