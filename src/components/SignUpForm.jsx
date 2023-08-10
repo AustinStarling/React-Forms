dee51fb11514de5d85b3ba71d9acffc4b785e1fb
austin <austin.starling347@gmail.com>
2023-08-10 17:37:56 -0400

    signupform & authenticate

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,17 +1,33 @@
 import { useState } from 'react'
 
-const [username, setUsername] = useState("")
-const [password, setPassword] = useState("")
-const [error, setError] = useState(null)
-
 export default function SignUpForm() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState(null);
+
+    async function handleSubmit(event) {
+        event.preventDefault();
+
+        try {
+            const response = await fetch("https://fsa-jwt-practice.herokuapp.com/signup", {
+            method: "POST",
+            body: JSON.stringify({ username, password})
+        });
+        
+        } catch (error) {
+            setError(error.message);
+        }
+        setToken(result.token);
+    };
+
     return <><h2>Sign Up!</h2>
-    <form>
+    {error && <p>{error}</p>}
+    <form onSubmit={handleSubmit}>
         <label>
-            Username: <input />
+            Username:{" "} <input value={username} onChange={(e) => setUsername(e.target.value)}/>
         </label>
         <label>
-            Password: <input />
+            Password:{" "} <input type="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
         </label>
         <button>Submit</button>
     </form></>;
